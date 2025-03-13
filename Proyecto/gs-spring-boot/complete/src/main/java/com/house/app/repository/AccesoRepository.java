20a74fa8f737ec413072cc829ab1814b17a0077a
Miguel02frf <146072927+Miguel02frf@users.noreply.github.com>
2025-03-13 12:34:16 +0100

    Delete Proyecto directory

--- Proyecto/gs-spring-boot/complete/src/main/java/com/house/app/repository/AccesoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.house.app.repository;
-
-import com.house.app.model.Acceso;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccesoRepository extends JpaRepository<Acceso, String> {
-}
--- Proyecto/gs-spring-boot/complete/src/main/java/com/house/app/repository/CerraduraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.house.app.repository;
-
-import com.house.app.model.Cerradura;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CerraduraRepository extends JpaRepository<Cerradura, String> {
-}
